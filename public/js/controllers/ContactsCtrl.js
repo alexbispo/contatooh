98f6c3fd0a39fcdddf423006eb897f2950211224
Alex Bispo <alexbispo.artes@gmail.com>
2020-03-24 07:02:12 +0000

    Takes bootstrap to the project.

--- public/js/controllers/ContactsCtrl.js
+++ public/js/controllers/ContactsCtrl.js
@@ -3,4 +3,12 @@ angular.module('contatooh').controller('ContactsCtrl', function($scope){
   $scope.increment = () => {
     $scope.total++;
   };
+
+  $scope.contacts = [
+    {_id: 1, name: 'Contato Exemplo 1', email: 'cont1@empresa.com.br'},
+    {_id: 2, name: 'Contato Exemplo 2', email: 'cont2@empresa.com.br'},
+    {_id: 3, name: 'Contato Exemplo 3', email: 'cont3@empresa.com.br'}
+  ];
+
+  $scope.filter = '';
 });
