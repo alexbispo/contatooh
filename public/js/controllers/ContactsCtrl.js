7dbf7768d188769495e4058ce726aa62347b27a4
Alex Bispo <alexbispo.artes@gmail.com>
2020-03-24 08:39:35 +0000

    Getting contacts from server.

--- public/js/controllers/ContactsCtrl.js
+++ public/js/controllers/ContactsCtrl.js
@@ -1,14 +1,13 @@
-angular.module('contatooh').controller('ContactsCtrl', function($scope){
-  $scope.total = 0;
-  $scope.increment = () => {
-    $scope.total++;
-  };
+angular.module('contatooh').controller('ContactsCtrl', function($scope, $http){
+  $scope.contacts = [];
 
-  $scope.contacts = [
-    {_id: 1, name: 'Contato Exemplo 1', email: 'cont1@empresa.com.br'},
-    {_id: 2, name: 'Contato Exemplo 2', email: 'cont2@empresa.com.br'},
-    {_id: 3, name: 'Contato Exemplo 3', email: 'cont3@empresa.com.br'}
-  ];
+  $http.get('/contacts')
+    .then(function(resp) {
+      $scope.contacts = resp.data;
+    }).catch(function(statusText) {
+      console.error('It was not possible get data from server');
+      console.log(statusText);
+    });
 
   $scope.filter = '';
 });
