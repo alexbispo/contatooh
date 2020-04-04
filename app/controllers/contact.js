eb824cbe3c13c96c1c60f515129d0bc2e4a7b921
Alex Bispo <alexbispo.artes@gmail.com>
2020-04-04 22:04:07 +0100

    Refactor to use angular-resource lib.

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -2,6 +2,7 @@ const contacts = [
   {_id: 1, name: 'Contato Exemplo 1', email: 'cont1@empresa.com.br'},
   {_id: 2, name: 'Contato Exemplo 2', email: 'cont2@empresa.com.br'},
   {_id: 3, name: 'Contato Exemplo 3', email: 'cont3@empresa.com.br'},
+  {_id: 4, name: 'Contato Exemplo 4', email: 'cont4@empresa.com.br'},
 ];
 
 module.exports = () => {
--- public/js/controllers/ContactsCtrl.js
+++ public/js/controllers/ContactsCtrl.js
@@ -1,13 +1,20 @@
-angular.module('contatooh').controller('ContactsCtrl', function($scope, $http){
-  $scope.contacts = [];
-
-  $http.get('/contacts')
-    .then(function(resp) {
-      $scope.contacts = resp.data;
-    }).catch(function(statusText) {
-      console.error('It was not possible get data from server');
-      console.log(statusText);
-    });
-
+angular.module('contatooh').controller('ContactsCtrl', function($scope, $resource){
   $scope.filter = '';
+  $scope.contacts = [];
+  $scope.totalContacts = 0;
+
+  const Contacts = $resource('/contacts');
+
+  Contacts.query(function(data) {
+    $scope.contacts = data;
+    $scope.totalContacts = data.length;
+
+  },
+  function(error) {
+    console.error('It was not possible get data from server');
+    console.error(error);
+
+    alert('Ocorreu um erro inesperado! Por favor, tente novamente mais tarde.');
+  });
+
 });
